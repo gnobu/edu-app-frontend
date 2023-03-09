b73cb6b3aa5ed029b52751051880aa5da01a38be
gnobu <inyangubong17@gmail.com>
2023-03-09 18:00:55 +0100

    classname returns empty instead of undefined

--- src/components/IconText.tsx
+++ src/components/IconText.tsx
@@ -3,7 +3,7 @@ type IconTextProps = {
 }
 export default function IconText({ src, srcCls, text, element, size, classNames }: IconTextProps) {
     return (
-        <div className={`f-gap-p5 aligned-flex ${size === 'xs' ? 'f-s-3' : 'f-s-4'} ${classNames}`} >
+        <div className={`f-gap-p5 aligned-flex ${size === 'xs' ? 'f-s-3' : 'f-s-4'} ${classNames ?? ''}`} >
             <svg className={srcCls} height={'1.5em'} >
                 <use href={src} width={'100%'} height={'100%'} />
             </svg>
